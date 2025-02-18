34560729ebe3105657a8a9f543fb901cb2891e57
akkoshak <abdulkarim.koshak@gmail.com>
2025-02-18 19:10:09 +0300

    [Feature] Dev Settings

--- AIChat/Core/Explore/ExploreView.swift
+++ AIChat/Core/Explore/ExploreView.swift
@@ -18,6 +18,15 @@ struct ExploreView: View {
     @State private var isLoadingPopular: Bool = true
     
     @State private var path: [NavigationPathOption] = []
+    @State private var showDevSettings: Bool = false
+    
+    private var showDevSettingsButton: Bool {
+        #if DEV || MOCK
+        return true
+        #else
+        return false
+        #endif
+    }
     
     var body: some View {
         NavigationStack(path: $path) {
@@ -43,6 +52,16 @@ struct ExploreView: View {
                 }
             }
             .navigationTitle("Explore")
+            .toolbar(content: {
+                ToolbarItem(placement: .topBarLeading) {
+                    if showDevSettingsButton {
+                        devSettingsButton
+                    }
+                }
+            })
+            .sheet(isPresented: $showDevSettings, content: {
+                DevSettingsView()
+            })
             .navigationDestinationForCoreModule(path: $path)
             .task {
                 await loadFeaturedAvatars()
@@ -51,6 +70,18 @@ struct ExploreView: View {
                 await loadPopularAvatars()
             }
         }
+    }
+    
+    private var devSettingsButton: some View {
+        Text("DEV 🔨")
+            .badgeButton()
+            .anyButton(.press) {
+                onDevSettingsPressed()
+            }
+    }
+    
+    private func onDevSettingsPressed() {
+        showDevSettings = true
     }
     
     private var loadingIndicator: some View {
